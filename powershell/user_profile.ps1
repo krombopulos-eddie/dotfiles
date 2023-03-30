651a48053971f7cfbc614675f8e730dfcfef8a43
Eddie rios <krombopulos.eddie@gmail.com>
2023-03-29 17:54:43 -0700

    first commit

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,31 @@
+$env:POSH_GIT_ENABLED = $true
+
+#Prompt
+oh-my-posh init pwsh | Invoke-Expression
+Import-Module posh-git
+Import-Module -Name Terminal-Icons
+oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH/eddie.omp.json" | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+h'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
